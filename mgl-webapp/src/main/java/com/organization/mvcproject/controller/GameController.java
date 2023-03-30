11221a37de05f2a26cae03f342714ba6e78e8a0e
Anthony <tony.j.hanson@gmail.com>
2023-03-30 14:56:10 -0500

    Split controller responsibility, working update function

--- mgl-webapp/src/main/java/com/organization/mvcproject/controller/GameController.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/controller/GameController.java
@@ -2,59 +2,77 @@ package com.organization.mvcproject.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.organization.mvcproject.api.model.GameInterface;
+import com.organization.mvcproject.api.service.GameService;
 import com.organization.mvcproject.model.Game;
-import com.organization.mvcproject.model.Review;
-import com.organization.mvcproject.service.GameService;
 
-@Controller
+@RequestMapping(value ="/game/")
+@RestController
 public class GameController {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(GameController.class);
 
 	@Autowired
-	private GameService javaGameService;
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-		return "index";
-	}
-	
-	@RequestMapping(value = "/review", method = RequestMethod.GET)
-	public ModelAndView review() {
-		return new ModelAndView("reviewCreatePage", "command", new Review());
-	}
-
-	@RequestMapping(value = "/addReview", method = RequestMethod.POST)
-	public ModelAndView addReview(Review review, ModelMap model) {
-		if(review.getAuthor().equals("")) {
-			review.setAuthor("anonymous");
-		}
-		return new ModelAndView("reviewDetailPage", "submittedReview", review);
-	}
+	private GameService gameService;
 
 	
-	@RequestMapping(value = "/games", method = RequestMethod.GET)
-	public ModelAndView game() {
-		return new ModelAndView("gamesPage", "command", new Game());
+
+	@GetMapping(value = "/")
+	public ResponseEntity<List<GameInterface>> fetchAllGames() {
+		return new ResponseEntity<List<GameInterface>>(gameService.retrieveAllGames(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/game", method = RequestMethod.GET)
-	public ResponseEntity<List<Game>> fetchAllGames() {
-		return new ResponseEntity<List<Game>>(javaGameService.retrieveAllGames(), HttpStatus.OK);
-	}
-
-	@RequestMapping(value = "/game", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> createGame(@RequestBody Game game) {
-		javaGameService.saveGame(game);
-		return new ResponseEntity<Void>(HttpStatus.CREATED);
+		gameService.saveGame(game);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
+	
+//	@RequestMapping(value = "/game", method = RequestMethod.GET)
+//	public ModelAndView game() {
+//		return new ModelAndView("gamesPage", "command", new Game());
+//	}
+	
+	
+	// Server requests are GET, POST, PUT, DELETE, ..... OPTIONS.... not as recognizable.
+	
+	@PutMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> updateGame(@RequestBody Game game){
+		
+		return new ResponseEntity<>(gameService.updateGame(game), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> deleteGame(@PathVariable("id") Long id){
+		return new ResponseEntity<>(gameService.deleteGame(id), HttpStatus.OK);
+		
+	}		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
