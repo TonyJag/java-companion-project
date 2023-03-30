526cabb9123af294be9f364e285273c8bc578381
Anthony <tony.j.hanson@gmail.com>
2023-03-30 14:44:40 -0500

    implemented Company interface

--- mgl-webapp/src/main/java/com/organization/mvcproject/model/Company.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/model/Company.java
@@ -1,37 +1,42 @@
 
 package com.organization.mvcproject.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import com.organization.mvcproject.api.model.CompanyInterface;
 
-public class Company {
+public class Company implements CompanyInterface {
 
 	private Long id;
 	private String name;
 	private List<Game> gamesMade;
 
+	@Override
 	public Long getId() {
 		return id;
 	}
 
+	@Override
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	@Override
 	public String getName() {
 		return name;
 	}
 
+	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	@Override
 	public List<Game> getGamesMade() {
 		return gamesMade;
 	}
 
+	@Override
 	public void setGamesMade(List<Game> gamesMade) {
 		this.gamesMade = gamesMade;
 	}
